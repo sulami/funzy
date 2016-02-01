d3fdd280da3a15a74f6068fc343d58b8ed6f6db6
Robin Schroer <sulami@peerwire.org>
2016-02-01 16:07:54 +0100

    start implementing the runner

--- src/Text/Funzy.hs
+++ src/Text/Funzy.hs
@@ -1,5 +1,11 @@
 module Text.Funzy where
 
+import           Data.List (sort)
+
 run :: IO ()
 run = return ()
+
+runner :: String -> [String] -> [String]
+runner _ [] = []
+runner _ xs= sort xs
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,5 @@
+import           Data.List       (sort)
+
 import           Test.Hspec
 import           Test.QuickCheck
 
@@ -14,3 +16,13 @@ main = hspec $ do
   describe "the main process" $
     it "exits gracefully" $
       run `shouldReturn` ()
+
+  describe "the runner" $ do
+    it "returns an empty list without any input data" $
+      property $
+        \x -> runner x [] `shouldBe` []
+
+    it "returns the sorted input if the search term is empty" $
+      property $
+        \x -> runner "" x `shouldBe` sort x
+
